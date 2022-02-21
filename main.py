06ff4d30e04144e0059cdaa14c11f9a8f8dc391b
VishnukantMule <20104065@apsit.edu.in>
2022-02-21 22:37:15 +0530

    raw commit

--- main.py
+++ main.py
@@ -1,9 +1,68 @@
-import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Dialog")
+        Dialog.resize(1100, 750)
+        self.frame = QtWidgets.QFrame(Dialog)
+        self.frame.setGeometry(QtCore.QRect(0, 0, 751, 471))
+        self.frame.setStyleSheet("background-color: rgb(255, 255, 255);")
+        self.frame.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.frame.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.frame.setObjectName("frame")
+        self.lineEdit = QtWidgets.QLineEdit(self.frame)
+        self.lineEdit.setGeometry(QtCore.QRect(370, 130, 291, 41))
+        self.lineEdit.setStyleSheet("background-color: rgb(255, 251, 192);")
+        self.lineEdit.setText("")
+        self.lineEdit.setObjectName("lineEdit")
+        self.lineEdit_2 = QtWidgets.QLineEdit(self.frame)
+        self.lineEdit_2.setGeometry(QtCore.QRect(370, 220, 291, 41))
+        self.lineEdit_2.setStyleSheet("background-color: rgb(255, 251, 192);")
+        self.lineEdit_2.setObjectName("lineEdit_2")
+        self.label = QtWidgets.QLabel(self.frame)
+        self.label.setGeometry(QtCore.QRect(380, 90, 141, 21))
+        font = QtGui.QFont()
+        font.setPointSize(16)
+        self.label.setFont(font)
+        self.label.setObjectName("label")
+        self.label_2 = QtWidgets.QLabel(self.frame)
+        self.label_2.setGeometry(QtCore.QRect(380, 190, 141, 21))
+        font = QtGui.QFont()
+        font.setPointSize(16)
+        self.label_2.setFont(font)
+        self.label_2.setObjectName("label_2")
+        self.pushButton = QtWidgets.QPushButton(self.frame)
+        self.pushButton.setGeometry(QtCore.QRect(370, 320, 301, 61))
+        font = QtGui.QFont()
+        font.setPointSize(19)
+        self.pushButton.setFont(font)
+        self.pushButton.setStyleSheet("background-color: rgb(87, 255, 69);")
+        self.pushButton.setObjectName("pushButton")
+        self.label_3 = QtWidgets.QLabel(self.frame)
+        self.label_3.setGeometry(QtCore.QRect(30, 80, 291, 291))
+        self.label_3.setStyleSheet("image: url(:/doctorpng/D:/Raw/—Pngtree—poster thank you doctors and_5349615.png);")
+        self.label_3.setText("")
+        self.label_3.setObjectName("label_3")
+
+        self.retranslateUi(Dialog)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        _translate = QtCore.QCoreApplication.translate
+        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
+        self.label.setText(_translate("Dialog", "username"))
+        self.label_2.setText(_translate("Dialog", "password"))
+        self.pushButton.setText(_translate("Dialog", "LOGIN"))
+
+
+
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    w = QWidget()
-    w.resize(300,300)
-    w.setWindowTitle("ClinicManagamentSystem-DASHBORD")
-    w.show()
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    Dialog = QtWidgets.QDialog()
+    ui = Ui_Dialog()
+    ui.setupUi(Dialog)
+    Dialog.show()
     sys.exit(app.exec_())
