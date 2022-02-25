8b59b6d571fee169663919e5dac1cccd6ec06512
Prathamesh Naik <naikprashant748@gmail.com>
2022-02-25 18:06:26 +0530

    Commit for Doctor_Login.py page

--- Doctor_Login.py
+++ Doctor_Login.py
@@ -1,144 +1,49 @@
 from tkinter import *
+from tkinter import messagebox
+from tkinter import messagebox
 
-
-class DoctorLogin:
+class Admin_Login:
     def __init__(self, root):
         self.root = root
-        self.root.title("DoctorLogin System")
+        self.root.title("Doctor_Login System")
         self.root.geometry("1030x750")
+        self.root.resizable(False, False)
 
-        root = Tk()
-        obj = DoctorLogin(root)
-        root.mainloop()
+        self.bg = PhotoImage(file="")
+        Label(self.root, image=self.bg).place(x=0, y=0, relwidth=1, relheight=1)
 
-# import modules
-from tkinter import *
-#import sqlite3
-import tkinter.messagebox
+        #Login Frame
+        Frame_login = Frame(self.root, bg="light green")
+        Frame_login.place(x=250, y=150, width=500, height=400)
 
-# connect to the databse.
-#conn = sqlite3.connect('database.db')
-# cursor to move around the databse
-#c = conn.cursor()
+        # ADMIN LOGIN
+        title = Label(Frame_login, text="DOCTOR LOGIN", font=("Impact", 35, "bold"), fg="Dark green", bg="light green").place(x=90,y=30)
+        subtitle = Label(Frame_login, text="LOGIN HERE", font=("goudy", 15, "bold"), fg="Dark green", bg="light green").place(x=90,y=100)
 
-# empty list to later append the ids from the database
-ids = []
+        # ID
+        lbl_user = Label(Frame_login, text="Id", font=("goudy", 15, "bold"), fg="Dark green", bg="light green").place(x=90,y=140)
 
+        self.idname = Entry(Frame_login,  font=("goudy old style", 15 ), bg="white")
+        self.idname.place(x=90,y=170,width=320,height=35)
 
-# tkinter window
-class Application:
-    def __init__(self, master):
-        self.master = master
+        lbl_password = Label(Frame_login, text="Password", font=("goudy", 15, "bold"), fg="Dark green", bg="lightgreen").place(x=90, y=210)
 
-        # creating the frames in the master
-        self.left = Frame(master, width=800, height=720, bg='lightgreen')
-        self.left.pack(side=LEFT)
+        self.password = Entry(Frame_login, font=("goudy old style", 15), bg="white")
+        self.password.place(x=90, y=240, width=320, height=35)
 
-        self.right = Frame(master, width=400, height=720, bg='steelblue')
-        self.right.pack(side=RIGHT)
-
-        # labels for the window
-        self.heading = Label(self.left, text="Apsit CLinic Appointments", font=('arial 40 bold'), fg='black',
-                             bg='lightgreen')
-        self.heading.place(x=0, y=0)
-        # patients name
-        self.name = Label(self.left, text="Patient's Name", font=('arial 18 bold'), fg='black', bg='lightgreen')
-        self.name.place(x=0, y=100)
-
-        # age
-        self.age = Label(self.left, text="Age", font=('arial 18 bold'), fg='black', bg='lightgreen')
-        self.age.place(x=0, y=140)
-
-        # gender
-        self.gender = Label(self.left, text="Gender", font=('arial 18 bold'), fg='black', bg='lightgreen')
-        self.gender.place(x=0, y=180)
-
-        # location
-        self.location = Label(self.left, text="Location", font=('arial 18 bold'), fg='black', bg='lightgreen')
-        self.location.place(x=0, y=220)
-
-        # appointment time
-        self.time = Label(self.left, text="Appointment Time", font=('arial 18 bold'), fg='black', bg='lightgreen')
-        self.time.place(x=0, y=260)
-
-        # phone
-        self.phone = Label(self.left, text="Phone Number", font=('arial 18 bold'), fg='black', bg='lightgreen')
-        self.phone.place(x=0, y=300)
-
-        # Entries for all labels============================================================
-        self.name_ent = Entry(self.left, width=30)
-        self.name_ent.place(x=250, y=100)
-
-        self.age_ent = Entry(self.left, width=30)
-        self.age_ent.place(x=250, y=140)
-
-        self.gender_ent = Entry(self.left, width=30)
-        self.gender_ent.place(x=250, y=180)
-
-        self.location_ent = Entry(self.left, width=30)
-        self.location_ent.place(x=250, y=220)
-
-        self.time_ent = Entry(self.left, width=30)
-        self.time_ent.place(x=250, y=260)
-
-        self.phone_ent = Entry(self.left, width=30)
-        self.phone_ent.place(x=250, y=300)
-
-        # button to perform a command
-        self.submit = Button(self.left, text="Add Appointment", width=20, height=2, bg='steelblue',
-                             command=self.add_appointment)
-        self.submit.place(x=300, y=340)
-
-        # getting the number of appointments fixed to view in the log
-  #      sql2 = "SELECT ID FROM appointments "
- #       self.result = c.execute(sql2)
-        for self.row in self.result:
-            self.id = self.row[0]
-            ids.append(self.id)
-
-        # ordering the ids
-        self.new = sorted(ids)
-        self.final_id = self.new[len(ids) - 1]
-        # displaying the logs in our right frame
-        self.logs = Label(self.right, text="Logs", font=('arial 28 bold'), fg='white', bg='steelblue')
-        self.logs.place(x=0, y=0)
-
-        self.box = Text(self.right, width=50, height=40)
-        self.box.place(x=20, y=60)
-        self.box.insert(END, "Total Appointments till now :  " + str(self.final_id))
-
-    # funtion to call when the submit button is clicked
-    def add_appointment(self):
-        # getting the user inputs
-        self.val1 = self.name_ent.get()
-        self.val2 = self.age_ent.get()
-        self.val3 = self.gender_ent.get()
-        self.val4 = self.location_ent.get()
-        self.val5 = self.time_ent.get()
-        self.val6 = self.phone_ent.get()
-
-        # checking if the user input is empty
-    #    if self.val1 == '' or self.val2 == '' or self.val3 == '' or self.val4 == '' or self.val5 == '':
-     #       tkinter.messagebox.showinfo("Warning", "Please Fill Up All Boxes")
-   #     else:
-            # now we add to the database
-      #      sql = "INSERT INTO 'appointments' (name, age, gender, location, scheduled_time, phone) VALUES(?, ?, ?, ?, ?, ?)"
-       #     c.execute(sql, (self.val1, self.val2, self.val3, self.val4, self.val5, self.val6))
-        #    conn.commit()
-         #   tkinter.messagebox.showinfo("Success", "Appointment for " + str(self.val1) + " has been created")
-
-          #  self.box.insert(END, 'Appointment fixed for ' + str(self.val1) + ' at ' + str(self.val5))
+        #BUTTON
+        forgetpassword = Button(Frame_login, text="forget password ?",bd=0,cursor="hand2",font=("goudy old style", 12),fg="darkgreen", bg="lightgreen").place(x=90,y=280)
+        submit = Button(Frame_login,command=self.check_function,cursor="hand2", text="LOGIN", bd=0, font=("goudy old style", 12), fg="light green",bg="Dark green").place(x=90, y=320,width=180 ,height=40)
 
 
-# creating the object
+
+    def check_function(self):
+         if self.idname.get() == "" or self.password.get() == "":
+             messagebox.showerror("Error", "All fields are required", parent=self.root)
+         elif self.idname.get() != "AVINASH" or self.password.get() != "123456":
+             messagebox.showerror("Error", "Invalid Username or Password", parent=self.root)
+         else:
+             messagebox.showerror("Welome", f"welcome {self.idname.get()}")
 root = Tk()
-b = Application(root)
-
-# resolution of the window
-root.geometry("1200x720+0+0")
-
-# preventing the resize feature
-root.resizable(False, False)
-
-# end the loop
+obj = Admin_Login(root)
 root.mainloop()
