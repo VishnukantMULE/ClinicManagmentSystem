36fd815911b908e30cf15086d7f7718318f5d61d
20104138Avinash <20104138.avinashandhale@gmail>
2022-02-27 19:13:14 +0530

    OTP confirmation in frame

--- EmailSender.py
+++ EmailSender.py
@@ -2,11 +2,11 @@
 import smtplib
 s = smtplib.SMTP('smtp.gmail.com', 587)
 s.starttls()
-s.login("vishnukantmule@gmail.com", "cgezqyarhsxghdfy")
+s.login("20104138.avinashandhale@gmail.com", "bbruwivjwtxqeozr")
 
-message = "YOU APPOINTMENT HAS BEEN BOOK "
+message = " Avinash Clinic 24/7 Management Dear Sir/ma'am, YOUR APPOINTMENT HAS BEEN BOOKED "
 
 
-s.sendmail("vishnukant@gmail.com", "20104109jeminbhanushali@gmail.com", message,)
+s.sendmail("20104138.avinashandhale@gmail.com", "20104109jeminbhanushali@gmail.com", message,)
 s.quit()
 
